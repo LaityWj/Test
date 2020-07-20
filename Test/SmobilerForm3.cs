3cebb98f39de99974fe3030bb1004ed93ee29109
2501023011 <2501023011@qq.com>
2020-07-20 17:14:50 +0800

    崔杰

---
+++ Test/SmobilerForm3.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Smobiler.Core;
+using Smobiler.Core.Controls;
+
+namespace Test
+{
+    partial class SmobilerForm3 : Smobiler.Core.Controls.MobileForm
+    {
+        public SmobilerForm3() : base()
+        {
+            //This call is required by the SmobilerForm.
+            InitializeComponent();
+        }
+
+        private void button1_Press(object sender, EventArgs e)
+        {
+            MessageBox.Show("王晋是一个傻子");
+        }
+    }
+}
